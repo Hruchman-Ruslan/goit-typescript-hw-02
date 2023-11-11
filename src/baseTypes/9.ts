638c6ed99e31c3d80cb0fbaf7637e2181b4b4045
Ruslan <Giovingo92@gmail.com>
2023-11-11 15:12:42 +0200

    fix: baseType: task-09, generic: task-04, -06

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -17,7 +17,7 @@ interface Page {
   title: string;
   likes: number;
   accounts: string[];
-  status: string;
+  status: "open" | "close";
   details?: {
     createAt: Date;
     updateAt: Date;
--- src/generic/6.ts
+++ src/generic/6.ts
@@ -14,15 +14,8 @@ type User = {
   password: string;
 };
 
-function createOrUpdateUser(initialValues: Partial<User>): User {
-  const defaultUser: User = {
-    name: "",
-    surname: "",
-    email: "",
-    password: "",
-  };
-
-  return { ...defaultUser, ...initialValues };
+function createOrUpdateUser(initialValues: Partial<User>) {
+  // Оновлення користувача
 }
 
 createOrUpdateUser({ email: "user@mail.com", password: "password123" });
